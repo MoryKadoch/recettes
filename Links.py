5b2f768aa48e083bf48be78c2ec0080bd5694475
MoryKadoch <moryykadoch@gmail.com>
2024-02-07 15:15:44 +0100

    Links

--- Links.py
+++ Links.py
@@ -2,6 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
-def get_links:
+def get_links():
+    url = "https://www.cuisineaz.com/categories/desserts-cat48681"
+
+    options = Options()
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(url)
+
+    try :
+        cookies = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "didomi-notice-agree-button")))
+        cookies.click()
+    except:
+        pass
     
+    links = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a.tile_title")))
+    
+    links = list(set(links))
+    
+    for link in links:
+        print(link.get_attribute("href"))
+    
+    return links
+    
+if __name__ == "__main__":
+    get_links()
